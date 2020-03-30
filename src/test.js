587c33c404c958ab8c80d178f63b215bc17d6942
BogdanAntonyuk <boba.tochka@gmail.com>
2020-03-30 14:21:38 +0300

    change

--- src/test.js
+++
@@ -1,6 +0,0 @@
-const fetch = require('node-fetch')
-let url = 'http://db2.yourhope.tv/api/episodes/ILCU01114?token=F[dcaEmr8TQkXFK9NshwBwoq';
-
-fetch('http://db2.yourhope.tv/api/episodes/ILCU01114?token=F[dcaEmr8TQkXFK9NshwBwoq')
-    .then(res => res.json())
-    .then(json => console.log(json.title));
