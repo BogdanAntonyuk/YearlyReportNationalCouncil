d217ab88d1a5d66393f6279ab18b1861706687bd
BogdanAntonyuk <boba.tochka@gmail.com>
2020-03-30 14:31:19 +0300

    some cosmetic changes

--- src/writeToExcel.js
+++ src/writeToExcel.js
@@ -1,55 +1,27 @@
 const xlsx = require('xlsx');
 const getEpisodes = require('./getAllEpisodes');
 
-
 const path = 'files/Report2019.xlsx';
 
-
-
-const readFile = async (path, sheetName) => {
-	const wb = xlsx.readFile(path);
-
-	const ws = wb.Sheets[sheetName];
-
-	const data = xlsx.utils.sheet_to_json(ws);
-	console.log(data);
-	return data;
-};
-
-const formDataForExcel = async (code) =>{
-    const data = await getEpisodes(code);
-    const newData=[];
-    for(i=0;i<data.length;i++){
-        newData.push({code: data[i].code, title:data[i].title})
-    }
-    return newData;
-}
-
-const getDataWithEpisodesNames = async (data, columnName) => {
+const formDataForExcel = async code => {
+	const data = await getEpisodes(code);
 	const newData = [];
-	try {
-		for (i = 0; i < data.length; i++) {
-			const title = await getEpisode(data[i][columnName]);
-			data[i].EpisodeName = title;
-			newData.push(data[i]);
-		}
-		console.log(newData)
-		return newData;
-	} catch (e) {
-		console.log(e);
+	for (i = 0; i < data.length; i++) {
+		newData.push({ code: data[i].code, title: data[i].title });
 	}
+	return newData;
 };
 
 const createExcelFile = async (searchCodeAndSheetNameToCreate, fileName) => {
 	try {
-		const dataForExcel = await formDataForExcel(searchCodeAndSheetNameToCreate)
+		const dataForExcel = await formDataForExcel(searchCodeAndSheetNameToCreate);
 		const newWB = xlsx.utils.book_new();
 		const newWS = xlsx.utils.json_to_sheet(dataForExcel);
 		xlsx.utils.book_append_sheet(newWB, newWS, searchCodeAndSheetNameToCreate);
-		xlsx.writeFile(newWB, 'files/'+fileName + '.xlsx');
+		xlsx.writeFile(newWB, 'files/' + fileName + '.xlsx');
 	} catch (e) {
 		console.log(e);
 	}
 };
 
-createExcelFile('CLMU', 'MusicVideos');
+createExcelFile('LOUU', 'Postcards(children)');
